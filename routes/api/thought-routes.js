cc90332b27e84ba1bfbd90a9500ed214d7ad0196
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-26 06:36:52 -0600

    createdThoughtsFile

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { 
+    getAllThoughts, 
+    getThoughtsById, 
+    createThoughts, 
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughts-controller');
