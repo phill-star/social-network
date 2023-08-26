77084fd90f8279091402db124947e7a1e1d8bc25
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-26 06:02:04 -0600

    creadtedMongooseConnecton

--- server.js
+++ server.js
@@ -12,3 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
+
+// Connect to MongoDB using mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Set mongoose to log queries for debugging
+mongoose.set('debug', true);
