57775ceab9eefcfd3ad03f4fc28d436a09a05874
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-26 08:12:08 -0600

    createdUpdateFunction

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -46,4 +46,22 @@ const thoughtsController = {
         console.log(err);
         res.sendStatus(400);
       });
+  },
+
+  updateThoughts({ params, body }, res) {
+    Thoughts.findOneAndUpdate(
+      { _id: params.id },
+      body,
+      { new: true, runValidators: true }
+    )
+      .populate({ path: 'reactions', select: '-__v' })
+      .select('-___v')
+      .then(dbThoughtsData => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: 'No thoughts with this particular ID!' });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch(err => res.json(err));
   }};
