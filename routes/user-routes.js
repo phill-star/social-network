6ebd7f4ec07cb491a67cc24d3e2c41c09cf87925
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-26 07:01:22 -0600

    addedFunctionforUser

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -8,3 +8,10 @@ const {
     addFriend,
     deleteFriend
 } = require('../../controllers/users-controller');
+
+
+router.route('/').get(getAllUsers).post(createUsers);
+router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
