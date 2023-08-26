9033bd18b7c8f1ec1ae6953e39f2974e1c9087c0
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-26 06:15:14 -0600

    addedStatusforError

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,10 @@ const apiRoutes = require('./api');
 
 // add prefix of `/api` to all of the api routes
 router.use('/api', apiRoutes);
+
+// 404 Status error message
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error....</h1>');
+  });
+
+module.exports = router;
